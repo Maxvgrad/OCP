afbe68867ab2d75ed8d55f94f54df1a08800d87a
Maksim Vinogradov <maxvgrad@gmail.com>
2019-01-10 08:23:20 +0300

    ch4: streams intermediate operations

--- src/test/java/org/ocp/streams/StreamTest.java
+++ src/test/java/org/ocp/streams/StreamTest.java
@@ -1,9 +1,15 @@
 package org.ocp.streams;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.ocp.dto.PersonDto;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,5 +55,78 @@ class StreamTest {
         Integer sumOfPersonId = Stream.of(buildPerson(), buildPerson(), buildPerson(), buildPerson())
                                              .reduce(0, (ident, person) -> ident + person.getId(), Math::addExact);
         assertEquals(Integer.valueOf(10), sumOfPersonId);
+    }
+
+    @Test
+    void collect3Args() {
+        List<Integer> personIdList = Stream.of(buildPerson(), buildPerson(), buildPerson(), buildPerson())
+                                           .collect(ArrayList::new, (arr, person) -> arr.add(person.getId()),
+                                                    ArrayList::addAll);
+        assertEquals(4, personIdList.size());
+    }
+
+    @Test
+    void collectArgs() {
+        List<Integer> personIdList = Stream.of(buildPerson(), buildPerson(), buildPerson(), buildPerson()).map(
+                PersonDto::getId).sorted().collect(Collectors.toCollection(ArrayList::new));
+        assertEquals(Integer.valueOf(1), personIdList.get(0));
+    }
+
+    @Test
+    void distinctTest() {
+        List<Integer> uniqNumbers = Stream.of(1, 1, 1, 1, 1).distinct().collect(Collectors.toList());
+        assertEquals(1, uniqNumbers.size());
+    }
+
+    @Test
+    void skipTest() {
+        Integer sum = Stream.of(1, 1, 1, 1, 2).skip(4).reduce(0, Integer::sum);
+        assertEquals(Integer.valueOf(2), sum);
+    }
+
+    @Test
+    void limitTest() {
+        Integer sum = Stream.of(1, 1, 1, 1, 2).limit(2).reduce(0, Integer::sum);
+        assertEquals(Integer.valueOf(2), sum);
+    }
+
+    @Test
+    void skipLimitTest() {
+        Stream<Integer> s = Stream.iterate(1, n -> n + 1);
+        s.skip(5).limit(2).reduce(Integer::sum).ifPresent((sum) -> assertEquals(Integer.valueOf(13), sum));
+    }
+
+    @Test
+    void flatMapTest() {
+        List<Integer> ints = Arrays.asList(1, 3, 5, 7);
+        List<Double> doubles = Arrays.asList(2.0, 4.0, 6.0, 8.0);
+        List<? extends Number> numbers = Stream.of(ints, doubles).flatMap(List::stream).collect(Collectors.toList());
+        assertEquals(8, numbers.size());
+    }
+
+    @Test
+    @DisplayName("Debug")
+    void debug1Test() {
+        Stream.generate(() -> "Elsa")
+              .filter(n -> n.length() == 4)
+              .limit(2)
+              .sorted()
+              .forEach(System.out::println);
+    }
+
+    @Test
+    @DisplayName("Debug")
+    void debug2Test() {
+        Stream.iterate(1, (n) -> n+1)
+              .limit(5)
+              .peek(System.out::print)
+              .filter(n -> n%2==1)
+              .forEach(System.out::print);
+        System.out.println();
+        Stream.iterate(1, (n) -> n+1)
+              .filter(n -> n%2==1)
+              .peek(System.out::print)
+              .limit(5)
+              .forEach(System.out::print);
     }
 }
---
+++ src/test/java/org/ocp/streams/collectors/CollectorsTest.java
@@ -0,0 +1,13 @@
+package org.ocp.streams.collectors;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+
+class CollectorsTest {
+
+    @Test
+    void test() {
+        Collectors.toList();
+    }
+}
