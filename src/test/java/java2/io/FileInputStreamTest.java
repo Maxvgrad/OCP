0b701f6e19d0e1a032bd3142500118b6139ac7c6
Maksim Vinogradov <maxvgrad@gmail.com>
2019-03-09 21:29:31 +0300

    ch8: Add InputStream mark/reset tests

--- src/test/java/java2/io/FileInputStreamTest.java
+++ src/test/java/java2/io/FileInputStreamTest.java
@@ -1,8 +1,55 @@
 package java2.io;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FileInputStreamTest {
 
-    private FileInputStream fileInputStream;
+    private InputStream fileInputStream;
+    private File tmp = IoHelper.createTmpFile();
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        fileInputStream = new BufferedInputStream(new FileInputStream(tmp));
+        IoHelper.fill("XYZABC", tmp);
+    }
+
+    @Test
+    void markSupported() {
+        assertTrue(fileInputStream.markSupported());
+    }
+
+    @Test
+    void markAndReset() throws Exception {
+        fileInputStream.mark(3);
+        int x = fileInputStream.read();
+        int y = fileInputStream.read();
+        int z = fileInputStream.read();
+        fileInputStream.reset();
+        assertEquals(x, fileInputStream.read());
+        assertEquals(y, fileInputStream.read());
+        assertEquals(z, fileInputStream.read());
+    }
+
+    @Test
+    void markExceedReadLimit() throws Exception {
+        fileInputStream.mark(1);
+        int x = fileInputStream.read();
+        int y = fileInputStream.read();
+        int z = fileInputStream.read();
+        int exceedingByte = fileInputStream.read();
+        fileInputStream.reset();
+        assertEquals(x, fileInputStream.read());
+        assertEquals(y, fileInputStream.read());
+        assertEquals(z, fileInputStream.read());
+    }
 }
